e279c85eb20baea0d6715e56744da2c96cd47ec0
dev-vkgoel <vkgoel1505@gmail.com>
2025-01-14 20:40:39 +0530

    Assigment Solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwonly = new RWOnly();
+
+        rwonly.setName("Birender");
+
+                System.out.println(rwonly.getName());
+                // 'name' has private access in 'com. driver. RWOnly'
+
+
+    }
   
 }
